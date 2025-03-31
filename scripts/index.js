535abf488803d1c581b86ff414d1e575c09784d5
ezekiel.lewis24 <ezekiel.lewis24@gmail.com>
2025-03-30 23:10:55 -0500

    Added script file for the card array

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://example.com/card1.jpg", 
+    },
+    {
+        name: "Lake Louise",
+        link: "https://example.com/card2.jpg", 
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://example.com/card3.jpg",
+    },
+    {
+        name: "Latemar",
+        link: "https://example.com/card4.jpg",
+    },
+    {
+        name: "Vanoise National Park",
+        link: "https://example.com/card5.jpg",
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://example.com/card6.jpg",
+    },
+];
+
+
+console.log(initialCards);
